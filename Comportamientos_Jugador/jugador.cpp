0675d93c2523a5aee55b0ce18b8caf9cab139be3
moonpyx <moonpyx6@gmail.com>
2024-04-21 20:18:21 +0200

    primeros cambios tutorial

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -6,15 +6,36 @@
 #include <set>
 #include <stack>
 
+list<Action> AvanzaASaltosDeCaballo()
+{
+	list<Action> secuencia;
+	secuencia.push_back(actWALK);
+	secuencia.push_back(actWALK);
+	secuencia.push_back(actTURN_L);
+	secuencia.push_back(actWALK);
+	return secuencia;
+}
+
+
 // Este es el método principal que se piden en la practica.
 // Tiene como entrada la información de los sensores y devuelve la acción a realizar.
 // Para ver los distintos sensores mirar fichero "comportamiento.hpp"
 Action ComportamientoJugador::think(Sensores sensores)
 {
 	Action accion = actIDLE;
-
-	// Incluir aquí el comportamiento del agente jugador
-
+	if (!hayPlan){
+		cout << "Calculamos un plan" << endl;
+		plan = AvanzaASaltosDeCaballo();
+		hayPlan=true;
+	}
+	if (hayPlan and plan.size()>0){
+		accion = plan.front();
+		plan.pop_front();;
+	}
+	if(plan.size()== 0){
+		cout << "Se completó el plan"<< endl;
+		hayPlan = false;
+	}
 	return accion;
 }
 
